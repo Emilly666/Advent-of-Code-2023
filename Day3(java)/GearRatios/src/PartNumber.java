646092eb5f22652efc5c3c1407e0eaa1636e8e71
Emilia Kalańska <127570572+Emilly666@users.noreply.github.com>
2024-08-21 20:50:00 +0200

    Finnished day 3 part 1

--- Day3(java)/GearRatios/src/PartNumber.java
+++ Day3(java)/GearRatios/src/PartNumber.java
@@ -3,15 +3,15 @@ import java.io.FileNotFoundException;
 import java.util.*;
 import java.util.regex.*;
 
-public class App {
+public class PartNumber {
     public static void main(String[] args) {
         // declarations
         List<String> inputTable = new ArrayList<>();
         List<Number> numberList = new ArrayList<>();
-        File myObj = new File("Day3(java)/test.txt");
+        File myObj = new File("Day3(java)/input.txt");
         Pattern numberPattern = Pattern.compile("(\\d+)");
         Pattern characterPattern = Pattern.compile("[^.\\d]");
-
+        int pattNumberSum = 0;
 
         // create list of Strings
         inputTable = initInput(myObj);
@@ -26,49 +26,31 @@ public class App {
             i++;
         }
         //iterate over list of Numbers and find out if they are part numbers
-
-        for (Number n : numberList){
-            //System.out.println(n);
-        }
-
         for (Number n : numberList){
             // check up
-            if(n.line != 0){
+            if(n.line > 0){
                 n.isPartNumber = characterPattern.matcher(inputTable.get(n.line - 1).substring(Math.max(0, n.indexStart-1), Math.min(inputTable.get(n.line - 1).length(), n.indexEnd + 1))).find();  
             }
             // check down
-            if(!n.isPartNumber && n.line != inputTable.size() -1){
+            if(!n.isPartNumber && n.line < inputTable.size() - 1 ){
                 n.isPartNumber = characterPattern.matcher(inputTable.get(n.line + 1).substring(Math.max(0, n.indexStart-1), Math.min(inputTable.get(n.line + 1).length(), n.indexEnd + 1))).find();  
             }
             //check left
-            if(!n.isPartNumber && n.indexStart != 0){
+            if(!n.isPartNumber && n.indexStart > 0){
                 n.isPartNumber = characterPattern.matcher( String.valueOf(inputTable.get(n.line).charAt(n.indexStart - 1))).find();
             }
              //check right
-             if(!n.isPartNumber && n.indexEnd != inputTable.get(n.line).length() - 1){
+            if(!n.isPartNumber && n.indexEnd < inputTable.get(n.line).length() - 1){
                 n.isPartNumber = characterPattern.matcher( String.valueOf(inputTable.get(n.line).charAt(n.indexEnd ))).find();
             }
         }
         for (Number n : numberList){
-            System.out.println(n.isPartNumber + " " + String.valueOf(n.value));
+            if (n.isPartNumber){
+                pattNumberSum += n.value;
+            }
         }
-
-
-
-
-        System.out.println(inputTable);
+        System.out.println(pattNumberSum);
     }
-
-
-
-
-
-
-
-
-
-
-
 
     static private class Number{
         int value = 0;
@@ -88,6 +70,7 @@ public class App {
             return "line: " + line + ", value: " + value + ", indexes: " + indexStart + "-" + indexEnd  + ", is part number: " + isPartNumber;
         }
     }
+
     private static List<String> initInput(File input) {
         List<String> returnList = new ArrayList<>();
         Scanner myReader;
