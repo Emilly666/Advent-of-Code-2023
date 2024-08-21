bb55a0d952201a59f2d785e137ed756ae82a5e4b
Emilia Kalańska <127570572+Emilly666@users.noreply.github.com>
2024-08-21 18:53:01 +0200

    created Number class

--- Day3(java)/GearRatios/src/App.java
+++ Day3(java)/GearRatios/src/App.java
@@ -1,39 +1,69 @@
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
+import java.util.regex.*;
 
 public class App {
     public static void main(String[] args) {
         // declarations
-        List<List<Character>> inputTable = new ArrayList<>();
-        File myObj = new File("Day3(java)/input.txt");
+        List<String> inputTable = new ArrayList<>();
+        File myObj = new File("Day3(java)/test.txt");
+        Pattern numberPattern = Pattern.compile("(\\d+)");
 
         // create list of lists of characters
         inputTable = initInput(myObj);
 
+        for (String line : inputTable){
+            Matcher matcher = numberPattern.matcher(line);
+
+            while (matcher.find()) {
+                List<Integer> idx = new ArrayList<>();
+                for (int i = matcher.start(); i < matcher.end(); i++){
+                    idx.add(i);
+                }
+
+                Number n = new Number(Integer.parseInt(matcher.group()), idx);
+                System.out.println(n.indexes);
+            }
+            System.out.println("\n");
+        }
+
+
+
+
         System.out.println(inputTable);
     }
 
-    private static List<List<Character>> initInput(File input) {
-        List<List<Character>> returnList = new ArrayList<>();
+    static private class Number{
+        int value = 0;
+        List<Integer> indexes = new ArrayList<>();
+        boolean isPartNumber = true;
+        
+        private Number(int val, List<Integer> idx){
+            value = val;
+            indexes = idx;
+        }
+    }
+
+
+
+
+
+
+
+    private static List<String> initInput(File input) {
+        List<String> returnList = new ArrayList<>();
         Scanner myReader;
         try {
             myReader = new Scanner(input);
             while (myReader.hasNextLine()) {
-                List<Character> line = new ArrayList<>();
-
-                for (char ch : myReader.nextLine().toCharArray()) {
-                    line.add(ch);
-                }
-                returnList.add(line);
+                returnList.add(myReader.nextLine());
             }
             myReader.close();
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-
         return returnList;
     }
-
 }
