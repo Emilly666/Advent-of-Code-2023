211f9735bd97621450f9c65586a88335b4daf451
Emilia Kalańska <127570572+Emilly666@users.noreply.github.com>
2024-08-22 22:24:18 +0200

    reading file

--- Day4(PLSQL)/Scratchcards.sql
+++ Day4(PLSQL)/Scratchcards.sql
@@ -1,14 +1,48 @@
 --set serveroutput on;
 
-CREATE DIRECTORY DIR__DAY4 AS 'C:/Repos/Advent-of-Code-2023/Day4(PLSQL)';
-/
+CREATE DIRECTORY DIR__DAY4 AS '/media/sf_Advent-of-Code-2023/Day4(PLSQL)';
+
+CREATE TABLE CARDS 
+(
+    CARD_NUMBER NUMBER NOT NULL PRIMARY KEY,
+    WINNING_NUMBERS VARCHAR2(150) NOT NULL,
+    CARD_NUMBERS VARCHAR2(150) NOT NULL,
+    WINNING_COUNT NUMBER
+);
+DROP TABLE CARDS;
+TRUNCATE TABLE CARDS;
+SELECT * FROM CARDS;
+
+-- rading input file and inserting into table
 DECLARE
-    file UTL_FILE.FILE_TYPE;
-    str VARCHAR2(150);
+    v_file UTL_FILE.FILE_TYPE;
+    v_line VARCHAR2(117);
+    v_card CARDS%ROWTYPE;
 
 BEGIN
-    file := UTL_FILE.FOPEN( 'DIR__DAY4', 'input.txt', 'R', 150 );
-    UTL_FILE.GET_LINE( file, str );
-    UTL_FILE.FCLOSE( file );
-    DBMS_OUTPUT.PUT_LINE( str );
+    v_file := UTL_FILE.FOPEN( 'DIR__DAY4', 'input.txt', 'R', 150 );
+    
+    LOOP
+        UTL_FILE.GET_LINE(v_file, v_line);
+        SELECT 
+            TO_NUMBER(REGEXP_SUBSTR (v_line, '(\S+):', 1, 1, NULL, 1)),
+            TRIM(REGEXP_SUBSTR (v_line, ':(.+)\|', 1, 1, NULL, 1)),
+            TRIM(REGEXP_SUBSTR (v_line, '\|(.+)', 1, 1, NULL, 1)),
+            NULL
+        INTO v_card
+        FROM dual;
+        
+        dbms_output.put_line(TO_CHAR(v_card.CARD_NUMBER) || ', ' || v_card.WINNING_NUMBERS || ' --- ' || v_card.CARD_NUMBERS);
+        
+        INSERT INTO CARDS VALUES v_card;
+        dbms_output.put_line('row inserted');
+        
+    END LOOP;
+    
+    EXCEPTION
+        WHEN no_data_found THEN
+            dbms_output.put_line('The whole file was read!');
+            UTL_FILE.FCLOSE(v_file);
 END;
+
+-- 
