448e232c1b997f35f9a66242b23c1af42d26b470
Emilia Kalańska <127570572+Emilly666@users.noreply.github.com>
2024-08-23 19:39:17 +0200

    Day 4 part 1 completed

--- Day4(PLSQL)/Scratchcards.sql
+++ Day4(PLSQL)/Scratchcards.sql
@@ -1,25 +1,33 @@
 --set serveroutput on;
 
-CREATE DIRECTORY DIR__DAY4 AS 'C:\Repos\Advent-of-Code-2023\Day4(PLSQL)';
---DROP DIRECTORY DIR__DAY4;
+-------------------------create schema level objects-----------------------------------------------
+CREATE OR REPLACE DIRECTORY DIR__DAY4 AS 'C:\Repos\Advent-of-Code-2023\Day4(PLSQL)';
+
+CREATE OR REPLACE TYPE T_CARD_NUMBERS AS TABLE OF NUMBER(2);
 
 CREATE TABLE CARDS 
 (
     CARD_NUMBER NUMBER NOT NULL PRIMARY KEY,
-    WINNING_NUMBERS VARCHAR2(150) NOT NULL,
-    CARD_NUMBERS VARCHAR2(150) NOT NULL,
-    WINNING_COUNT NUMBER
-);
---DROP TABLE CARDS;
---TRUNCATE TABLE CARDS;
---SELECT * FROM CARDS;
+    WINNING_NUMBERS T_CARD_NUMBERS,
+    CARD_NUMBERS T_CARD_NUMBERS,
+    WINNING_COUNT NUMBER,
+    POINTS NUMBER
+)
+    NESTED TABLE WINNING_NUMBERS STORE AS WINNING_NUMBERS_TABLE,
+    NESTED TABLE CARD_NUMBERS STORE AS CARD_NUMBERS_TABLE
+;
+/*
+SELECT * FROM CARDS;
+DROP TABLE CARDS;
+TRUNCATE TABLE CARDS;
+COMMIT;
+*/
 
 -------------------------execute function----------------------------------------------------------
 BEGIN
     EXECUTE IMMEDIATE ('TRUNCATE TABLE TEST.CARDS');
     dbms_output.PUT_LINE(SCRATCHCARDS.COUNT_WINNINGS('input.txt'));
 END;
-
 /
 -------------------------package definition--------------------------------------------------------
 CREATE OR REPLACE PACKAGE SCRATCHCARDS AS
@@ -28,41 +36,92 @@ END SCRATCHCARDS;
 /
 -------------------------package body definition---------------------------------------------------
 CREATE OR REPLACE PACKAGE BODY SCRATCHCARDS AS
+-------------------------converting string to table of numbers-------------------------------------
+    FUNCTION TO_CARD_NUMBERS(text VARCHAR2) RETURN T_CARD_NUMBERS AS
+        v_return T_CARD_NUMBERS := T_CARD_NUMBERS();
+    BEGIN
+        FOR i IN 1..REGEXP_COUNT(text, '[^ ]+') LOOP
+            v_return.EXTEND;
+            v_return(i) := TO_NUMBER(REGEXP_SUBSTR(text, '[^ ]+', 1, i));
+
+        END LOOP;
+        RETURN(v_return);
+
+    END TO_CARD_NUMBERS;
 -------------------------reading file into table---------------------------------------------------
     PROCEDURE READ_FILE(file_name VARCHAR2) AS
         v_file UTL_FILE.FILE_TYPE;
         v_line VARCHAR2(117);
         v_card CARDS%ROWTYPE;
-
+        v_temp VARCHAR2(100);
     BEGIN
         v_file := UTL_FILE.FOPEN( 'DIR__DAY4', file_name, 'R', 150 );
     
         LOOP
             UTL_FILE.GET_LINE(v_file, v_line);
-            SELECT 
-                TO_NUMBER(REGEXP_SUBSTR (v_line, '(\S+):', 1, 1, NULL, 1)),
-                TRIM(REGEXP_SUBSTR (v_line, ':(.+)\|', 1, 1, NULL, 1)),
-                TRIM(REGEXP_SUBSTR (v_line, '\|(.+)', 1, 1, NULL, 1)),
-                NULL
-            INTO v_card
+            -- get card number
+            SELECT REGEXP_SUBSTR(v_line, '(\S+):', 1, 1, NULL, 1)
+            INTO v_card.CARD_NUMBER
             FROM dual;
-            
-            dbms_output.put_line(TO_CHAR(v_card.CARD_NUMBER) || ', ' || v_card.WINNING_NUMBERS || ' --- ' || v_card.CARD_NUMBERS);
-            
-            INSERT INTO CARDS VALUES v_card;
-            dbms_output.put_line('row inserted');
-            
+
+            -- get winning numbers
+            SELECT TRIM(REGEXP_SUBSTR(v_line, ':(.+)\|', 1, 1, NULL, 1))
+            INTO v_temp
+            FROM dual;
+            v_card.WINNING_NUMBERS := TO_CARD_NUMBERS(v_temp);
+
+            --get card numbers
+            SELECT TRIM(REGEXP_SUBSTR (v_line, '\|(.+)', 1, 1, NULL, 1))
+            INTO v_temp
+            FROM dual;
+            v_card.CARD_NUMBERS := TO_CARD_NUMBERS(v_temp);    
+
+            INSERT INTO CARDS (CARD_NUMBER, WINNING_NUMBERS, CARD_NUMBERS, WINNING_COUNT)
+            VALUES (v_card.CARD_NUMBER, v_card.WINNING_NUMBERS, v_card.CARD_NUMBERS, 0);
+
         END LOOP;
         
         EXCEPTION
             WHEN no_data_found THEN
                 dbms_output.put_line('The whole file was read!');
                 UTL_FILE.FCLOSE(v_file);
+
     END READ_FILE;
+-------------------------calculate winnings for each row-------------------------------------------
+    PROCEDURE CALCULATE_WINNINGS AS
+        CURSOR c_cards IS SELECT * FROM CARDS FOR UPDATE;
+        v_row_win_count NUMBER;
+    BEGIN
+        FOR row in c_cards LOOP
+            v_row_win_count := 0;
+            FOR x in 1..row.CARD_NUMBERS.COUNT() LOOP
+                IF row.CARD_NUMBERS(x) MEMBER OF row.WINNING_NUMBERS THEN
+                    v_row_win_count := v_row_win_count + 1;
+                END IF;
+            END LOOP;
+
+            UPDATE CARDS SET WINNING_COUNT = v_row_win_count
+            WHERE CURRENT OF c_cards;
+
+            IF v_row_win_count > 0 THEN
+                UPDATE CARDS SET POINTS = POWER(2, v_row_win_count - 1)
+                WHERE CURRENT OF c_cards;
+            ELSE
+                UPDATE CARDS SET POINTS = 0
+                WHERE CURRENT OF c_cards;
+            END IF;
+
+        END LOOP;
+    END CALCULATE_WINNINGS;
 -------------------------main function-------------------------------------------------------------
     FUNCTION COUNT_WINNINGS(file_name VARCHAR2) RETURN NUMBER AS
+        v_sum NUMBER := 0;
     BEGIN
         SCRATCHCARDS.READ_FILE(file_name);
-        RETURN(1);
+        SCRATCHCARDS.CALCULATE_WINNINGS;
+
+        SELECT SUM(POINTS) INTO v_sum FROM CARDS;
+
+        RETURN(v_sum);
     END COUNT_WINNINGS;
 END SCRATCHCARDS;
