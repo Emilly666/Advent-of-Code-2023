17d16fb0fba3f1675452b0b599227c507663bb17
Emilia Kalańska <127570572+Emilly666@users.noreply.github.com>
2024-08-26 14:12:16 +0200

    Day10 start

--- Day10/pipes.py
+++ Day10/pipes.py
@@ -1,16 +1,26 @@
 from pathlib import Path
+from pprint import pprint
 
 def main():
-    print(f("test.txt"))
+    pprint(f("test.txt"))
 
 def f(filename = "input.txt"):
     text = initialize(filename)
-    return text
+    map = [[*line] for line in text]
+    start_index = find_S(map)
+    return start_index, map[start_index[0]][start_index[1]]
 
 def initialize(filename):
     with open(Path(__file__).parent / filename, "r") as file:
         lines = file.read()
     return lines
+
+def find_S(map):
+    for i, row in enumerate(map):
+        for j, element in enumerate(row):
+            if element == 'S':
+                return (i, j)
+    return None
 
 if __name__ == "__main__":
     main()
