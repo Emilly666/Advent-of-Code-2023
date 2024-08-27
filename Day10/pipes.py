655cb727a13bfd46b4dcf28ea4bec69a9bbb6d7b
Emilia Kalańska <127570572+Emilly666@users.noreply.github.com>
2024-08-27 12:59:29 +0200

    Day 10

--- Day10/pipes.py
+++ Day10/pipes.py
@@ -2,37 +2,7 @@ from pathlib import Path
 from pprint import pprint
 
 def main():
-    pprint(pipes_area("test.txt"))
-
-def pipes_area(filename = "input.txt"):
-    text = initialize(filename)
-    map = [[*line] for line in text]
-    steps = 0
-    directions = {
-        "down"  : { "|" : "down",  "L" : "right", "J" : "left", "S": "down" },
-        "left"  : { "-" : "left",  "L" : "up",    "F" : "down"  },
-        "up"    : { "|" : "up",    "7" : "left",  "F" : "right" },
-        "right" : { "-" : "right", "J" : "up",    "7" : "down"  }
-    }    
-    position = find_S(map)
-    go_next = "down"
-
-    while map[position[0]][position[1]] in ["|", "-", "L", "J", "7", "F", "S"]:
-        
-        map[position[0]][position[1]] = '#'
-        match go_next:
-            case "down":
-                position[0] += 1
-            case "left":
-                position[1] -= 1
-            case "up":
-                position[0] -= 1
-            case "right":
-                position[1] += 1
-        steps +=1
-        if map[position[0]][position[1]] == '#':
-            return map
-        go_next = directions[go_next][map[position[0]][position[1]]]
+    pprint(pipes())
 
 def pipes(filename = "input.txt"):
     text = initialize(filename)
