0e526806e79d125e6bec29b4a6f06b8c8f25fc22
Kalańska Emilia <kalanskae@gmail.com>
2025-02-13 08:10:12 +0100

    day 21 end

--- day21/part1.py
+++ day21/part1.py
@@ -1,19 +1,19 @@
 from pathlib import Path
-from collections import deque
 
-directions = [(-1, 0), (1, 0), [0, -1], (0, 1)]
-file = open(Path(__file__).parent / 'test.txt', "r").read().split('\n')
+directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+file = open(Path(__file__).parent / 'input.txt', "r").read().split('\n')
 start = [(ix,iy) for ix, row in enumerate(file) for iy, i in enumerate(row) if i == 'S'][0]
+cols = len(file[0])
+rows = len(file)
 
-possible = deque(start)
+possible = {start}
 
 for _ in range(64):
-    for position in possible:
-        
+    t = set()
+    for pos in possible:
         for d in directions:
-            if file[position[0] + d[0]][position[1] + d[1]] != '#':
-                possible.append((position[0] + d[0], position[1] + d[1]))
-
-    
+            if cols >= pos[0] + d[0] >= 0 and rows >= pos[1] + d[1] >= 0 and file[pos[0] + d[0]][pos[1] + d[1]] != '#':
+                t.add((pos[0] + d[0], pos[1] + d[1]))
+    possible = t
 
 print(len(possible))
